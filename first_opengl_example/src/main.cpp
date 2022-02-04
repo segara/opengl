f2b3561c596b1a430a0644369ad6bdc53c688a70
segara <segara@naver.com>
2022-02-04 19:16:03 +0900

    Render() 함수 추가

--- first_opengl_example/src/main.cpp
+++ first_opengl_example/src/main.cpp
@@ -2,6 +2,31 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void Render() {
+    glClearColor(0.1f, 0.2f, 0.3f, 0.0f); //프레임 버퍼 화면을 클리어시 쓸 색상
+    glClear(GL_COLOR_BUFFER_BIT); //프레임버퍼 클리어(GL_COLOR_BUFFER_BIT:색상버퍼를 지움)
+}
+
+void OnFramebufferSizeChange(GLFWwindow* window, int width, int height) {
+    SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
+    glViewport(0, 0, width, height); //open gl이 그림을 그릴 화면의 위치및 크기 설정
+}
+
+void OnKeyEvent(GLFWwindow* window,
+    int key, int scancode, int action, int mods) {
+    SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
+        key, scancode,
+        action == GLFW_PRESS ? "Pressed" :
+        action == GLFW_RELEASE ? "Released" :
+        action == GLFW_REPEAT ? "Repeat" : "Unknown",
+        mods & GLFW_MOD_CONTROL ? "C" : "-",
+        mods & GLFW_MOD_SHIFT ? "S" : "-",
+        mods & GLFW_MOD_ALT ? "A" : "-");
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
 int main(int argc, const char** argv)
 {
     SPDLOG_INFO("start program");
@@ -40,13 +65,18 @@ int main(int argc, const char** argv)
 
     auto glVersion = glGetString(GL_VERSION);
     SPDLOG_INFO("OpenGL context version :{}", glVersion);
-
+    OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
+    glfwSetKeyCallback(window, OnKeyEvent);
     //glfw 루프 실행, 윈도우 close시 종료
     SPDLOG_INFO("start main loop");
     while(!glfwWindowShouldClose(window))
     {
-    
         glfwPollEvents(); 
+        Render();
+        glfwSwapBuffers(window);
+
+        // glfwPollEvents
         // keyboard event, mouse event, window size 변경등의 event 수집 
         // window에 대한 처리만(렌더링x)
         //glfwPollEvents 내부는 windows인 경우 : while(peekmessage) 로 구현 -> DispatchMessage : WM_DESTROY 등의 메세지를 메세지 처리함수로 보냄
